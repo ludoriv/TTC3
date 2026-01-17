174ea0a470f25c65d063e7f97130201c4d8687ea
ludoriv <30841108+ludoriv@users.noreply.github.com>
2026-01-17 14:47:59 +0100

    Supp. des fichiers de travail

--- 2019-2021/2020_StGildas_2/Listimage.py
+++
@@ -1,9 +0,0 @@
-# import os ; os.chdir("C:/Comm2025/TTC_site/Site_TTCnew/2020_StGildas")# 
-# Dossier contenant les images
-dossier_images = "Sept_Dec_2020s"
-import os
-# Tableau contenant les noms des fichiers .jpg
-Photo = [f for f in os.listdir(dossier_images) if f.lower().endswith('.jpg')]
-
-# Affichage
-print(Photo)
